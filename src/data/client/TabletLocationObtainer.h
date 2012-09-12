a94a2b7509553065c72a9cf2fd734b38b25acd95
Marc <marc@binarystream.net>
2012-09-11 20:42:59 -0400

    incremental commit, before going out

--- src/data/client/TabletLocationObtainer.h
+++ src/data/client/TabletLocationObtainer.h
@@ -8,14 +8,29 @@
 #ifndef TABLETLOCATIONOBTAINER_H_
 #define TABLETLOCATIONOBTAINER_H_
 
+
+#include <list>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#include "../constructs/KeyExtent.h"
+#include "../constructs/Range.h"
 #include "ExtentLocator.h"
+#include "TabletLocation.h"
 namespace cclient {
 namespace impl {
 
+using namespace cclient::data;
 class TabletLocationObtainer : public TabletLocator{
 public:
 	TabletLocationObtainer();
+
 	virtual ~TabletLocationObtainer();
+
+	virtual list<TabletLocation> findTablet(TabletLocation *source, string row, string stopRow,TabletLocator *parent) = 0;
+	virtual list<TabletLocation> findTablet(string tabletserver, map<KeyExtent,list<Range>> *map,TabletLocator *parent) = 0;
 
 };
 
