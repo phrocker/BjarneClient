279c1bc84fe301b52c36f8c1f8a2f3fb6ddbc5af
Marc Parisi <marc@binarystream.net>
2012-10-13 16:38:53 -0400

    committing

---
+++ src/data/client/impl/metadata/MetaDataLocationObtainer.cpp
@@ -0,0 +1,18 @@
+/*
+ * MetaDataLocationObtainer.cpp
+ *
+ *  Created on: Oct 7, 2012
+ *      Author: marc
+ */
+
+#include "MetaDataLocationObtainer.h"
+
+namespace cclient {
+namespace impl {
+
+MetaDataLocationObtainer::~MetaDataLocationObtainer() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace impl */
+} /* namespace cclient */
