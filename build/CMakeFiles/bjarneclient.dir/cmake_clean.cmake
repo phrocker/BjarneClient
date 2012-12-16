90ad75129d823ad9bcf2b093dd33a86d807ee699
marc <marc@dev01.(none)>
2012-12-15 23:11:18 -0500

    initial project import from KDevelop

---
+++ build/CMakeFiles/bjarneclient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/bjarneclient.dir/main.o"
+  "bjarneclient.pdb"
+  "bjarneclient"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/bjarneclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
